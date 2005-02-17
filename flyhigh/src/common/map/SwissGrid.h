e9ed491732546837c15de211c44834716d831c81
(no author) <(no author)@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2005-02-17 20:18:58 +0000

    Standard project directories initialized by cvs2svn.
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@487 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- flyhigh/src/common/map/SwissGrid.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SwissGrid_h_
-#define _SwissGrid_h_
-
-void LLtoSwissGrid(const double Lat, const double Long,
-			 double &SwissNorthing, double &SwissEasting);
-void SwissGridtoLL(const double SwissNorthing, const double SwissEasting, 
-					double& Lat, double& Long);
-
-#endif
