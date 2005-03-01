9fc1dcc4a88086f57f897fecabec97daa18454a5
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2005-03-01 22:27:06 +0000

    print support
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@528 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/form/TableWindow.h
+++ src/common/form/TableWindow.h
@@ -7,6 +7,8 @@
 #ifndef _TableWindow_h_
 #define _TableWindow_h_
 
+#include <qdom.h>
+#include <qprinter.h>
 #include "MDIWindow.h"
 
 class QStringList;
@@ -15,19 +17,23 @@ class QTable;
 class TableWindow: public MDIWindow
 {
 	Q_OBJECT
+	
 	public:
 		TableWindow(QWidget* parent, const char* name, int wflags);
 		virtual ~TableWindow();
 
+		void tableAsHTML(QDomDocument &doc);
+
+	protected slots:
+		void print();
+		
 	protected:
 		QTable* getTable();
 		void setupHeader(const QStringList &colNameList);
 
-	protected slots:
-		virtual void clicked(int row, int col, int button, const QPoint &mousePos);
-
 private:
 	QTable* m_pTable;
+	QPrinter m_printer;
 };
 
 #endif
