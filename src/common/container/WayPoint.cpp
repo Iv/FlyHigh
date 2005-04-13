a3b5b2e235bc69c33f1636a421552e20bb2d8037
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2005-04-13 21:24:14 +0000

    arc(), meters() new
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@580 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/container/WayPoint.cpp
+++ src/common/container/WayPoint.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2004 by grafal,,,                                       *
- *   grafal@spirit                                                         *
+ *   Copyright (C) 2004 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -22,6 +22,7 @@
 #include "WayPoint.h"
 
 const uint WayPoint::startLandRadius = 500;
+const double WayPoint::earthRadius = 6371000.0;
  
 WayPoint::WayPoint()
 {
@@ -81,25 +82,7 @@ int WayPoint::altitude()
 
 uint WayPoint::distance(WayPoint &wp)
 {
-	double dist;
-	double angle; // in radians
-	double lat1; // in radians
-	double lat2; // in radians
-	double deltaLong; // in radians
-
-	// P1, L1 Latitude and Longitude of Point 1
-	// P2, L2 Latitude and Longitude of Point 2
-	lat1 = wp.latitude() * M_PI / 180;
-	lat2 = m_lat * M_PI / 180;
-	deltaLong = (m_lon - wp.longitude()) * M_PI / 180;
-	
-	// cos E = sin P1 * sin P2 + cos P1 * cos P2 * cos (L2 - L1)
-	angle = sinl(lat1) * sinl(lat2) + cosl(lat1) * cosl(lat2) * cosl(deltaLong);
-
-	// FAI Earth Radius is 6371 km
-	dist = 6371000.0 * acosl(angle);
-	
-	return (uint)round(dist);
+	return distance(wp, *this);
 }
 
 WayPoint& WayPoint::operator=(const WayPoint &wp)
@@ -111,4 +94,41 @@ WayPoint& WayPoint::operator=(const WayPoint &wp)
 	m_alt = wp.m_alt;
 	
 	return *this;
+}
+
+uint WayPoint::distance(WayPoint &wp1, WayPoint &wp2)
+{
+	double dist;
+	double angle; // in radians
+	double lat1; // in radians
+	double lat2; // in radians
+	double deltaLong; // in radians
+	
+	// P1, L1 Latitude and Longitude of Point 1
+	// P2, L2 Latitude and Longitude of Point 2
+	lat1 = wp1.latitude() * M_PI / 180;
+	lat2 = wp2.latitude() * M_PI / 180;
+	deltaLong = (wp2.longitude() - wp1.longitude()) * M_PI / 180;
+	
+	// cos E = sin P1 * sin P2 + cos P1 * cos P2 * cos (L2 - L1)
+	angle = sinl(lat1) * sinl(lat2) + cosl(lat1) * cosl(lat2) * cosl(deltaLong);
+
+	// FAI Earth Radius is 6371 km
+	dist = earthRadius * acosl(angle);
+	
+	return (uint)round(dist);
+}
+
+double WayPoint::arc(uint distance)
+{
+	double a;
+	
+	a = distance / earthRadius;
+	
+	return (a * 180.0 / M_PI);
+}
+
+double WayPoint::meters(double nautmil)
+{
+	return 1852.0 * nautmil;
 }
