c20650750dcad46c42d3e55e72bbbcabfeff11eb
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2005-06-01 20:46:34 +0000

    progress bar
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@230 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- flyhigh/src/common/form/MDIWindow.cpp
+++ flyhigh/src/common/form/MDIWindow.cpp
@@ -4,6 +4,9 @@
 	for more information.
 */
 
+#include <qapplication.h>
+#include <qeventloop.h>
+#include <qprogressdialog.h>
 #include <qwidget.h>
 #include <qpopupmenu.h>
 #include <qstring.h>
@@ -13,17 +16,14 @@
 #define UPDATE_TIME 1000 // 1 second
 
 MDIWindow::MDIWindow(QWidget* parent, const char* name, int wflags)
-   :QMainWindow(parent, name, wflags)
+	:QMainWindow(parent, name, wflags)
 {
+	m_pProgressDlg = NULL;
 	m_pUpdateTimer = new QTimer(this);
 	connect(m_pUpdateTimer, SIGNAL(timeout()), this, SLOT(updateTimeout()));
 	m_pUpdateTimer->start(0, true);
 }
 
-MDIWindow::~MDIWindow()
-{
-}
- 
 bool MDIWindow::periodicalUpdate()
 {
 	return false;
@@ -35,4 +35,33 @@ void MDIWindow::updateTimeout()
 	{
 		m_pUpdateTimer->start(UPDATE_TIME, true);
 	}
+}
+
+void MDIWindow::connectProgressDlg(const QString &label, QObject *pProgressObj)
+{
+	if(m_pProgressDlg != NULL)
+	{
+		delete m_pProgressDlg;
+	}
+	
+	m_pProgressDlg = new QProgressDialog(label, "Cancel", 100, this, "progress", true);
+	m_pProgressDlg->setAutoClose(false);
+	
+	connect(pProgressObj, SIGNAL(progress(int)), this, SLOT(progress(int)));
+	connect(m_pProgressDlg, SIGNAL(canceled()), pProgressObj, SLOT(cancel()));
+	
+	m_pProgressDlg->show();
+}
+
+void MDIWindow::disconnectProgressDlg()
+{
+//	m_pProgressDlg->hide();
+	delete m_pProgressDlg;
+	m_pProgressDlg = NULL;
+}
+
+void MDIWindow::progress(int percent)
+{
+	m_pProgressDlg->setProgress(percent);
+	QApplication::eventLoop()->processEvents(QEventLoop::AllEvents);
 }
