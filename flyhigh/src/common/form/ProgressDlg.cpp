354e72e2203f4e8ab6d2fd0425342041acb94d64
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2005-06-02 19:36:34 +0000

    initial
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@238 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ flyhigh/src/common/form/ProgressDlg.cpp
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Alex Graf                                     *
+ *   grafal@sourceforge.net                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+ 
+#include <qapplication.h>
+#include <qeventloop.h>
+#include <qprogressdialog.h>
+#include "ProgressDlg.h"
+
+ProgressDlg::ProgressDlg(QWidget *pCreator)
+{
+	m_pCreator = pCreator;
+	m_pProgDlg = NULL;
+}
+
+ProgressDlg::~ProgressDlg()
+{
+	endProgress();
+}
+
+void ProgressDlg::beginProgress(const QString &label, QObject *pProgObj)
+{
+	endProgress();
+	
+	m_pProgDlg = new QProgressDialog(label, "Cancel", 100, m_pCreator, "progress", true);
+	
+	connect(pProgObj, SIGNAL(progress(int)), this, SLOT(progress(int)));
+	connect(m_pProgDlg, SIGNAL(canceled()), pProgObj, SLOT(cancel()));
+
+	m_pProgDlg->setAutoClose(false);
+	m_pProgDlg->show();
+}
+
+void ProgressDlg::endProgress()
+{
+	if(m_pProgDlg != NULL)
+	{
+		delete m_pProgDlg;
+		m_pProgDlg = NULL;
+	}
+}
+
+void ProgressDlg::progress(int percent)
+{
+	m_pProgDlg->setProgress(percent);
+	QApplication::eventLoop()->processEvents(QEventLoop::AllEvents);
+}
