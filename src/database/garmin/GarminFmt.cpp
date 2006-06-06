3cd96ccd69c1186f825e394254f66eaeda06a80a
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2006-06-06 16:41:47 +0000

    missing #include <math.h> added
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@796 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/database/garmin/GarminFmt.cpp
+++ src/database/garmin/GarminFmt.cpp
@@ -18,6 +18,7 @@
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
 
+#include <math.h>
 #include <qbuffer.h>
 #include "GarminFmt.h"
 
