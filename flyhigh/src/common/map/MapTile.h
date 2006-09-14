1e77c131ff8c353e3cf5aa0e599a80d3ce58ec75
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2006-09-14 17:34:35 +0000

    Initial
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@314 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ flyhigh/src/common/map/MapTile.h
@@ -0,0 +1,22 @@
+#ifndef MapTile_h
+#define MapTile_h
+
+#include <qimage.h>
+
+class MapTile
+{
+	public:
+		MapTile(const QString &name, uint sizeX, uint sizeY);
+		~MapTile();
+
+		int width();
+		int height();
+		void setZoom(int z);
+		QImage& image();
+
+	private:
+		QImage m_image;
+		QImage m_scaledImage;
+};
+
+#endif
---
+++ flyhigh/src/common/map/MapView.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef _MapView_h
+#define _MapView_h
+
+class Map;
+
+#include <vector>
+#include <qscrollview.h>
+#include <qpixmap.h>
+#include <qpointarray.h>
+#include "WayPoint.h"
+
+class MapView: public QScrollView
+{
+	public:
+		MapView(QWidget *parent=0, const char *name=0, int wFlags=0);
+		virtual ~MapView();
+
+		void shwoWayPointList(WayPoint::WayPointListType &wpList);
+
+	protected:
+		void keyPressEvent(QKeyEvent * e);
+		void drawContents(QPainter* p, int cx, int cy, int cw, int ch);
+
+	private:
+		Map *m_pMap;
+		WayPoint::WayPointListType m_wpList;
+		QPointArray m_wayPoints;
+
+		void recalcWayPoints();
+};
+
+#endif
