1c7d4e38cd0102a8bac1555ed0a423b823e86349
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2010-03-17 20:54:32 +0000

    FA functions reduced to read/writeMemory()
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@989 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/database/flytec6015/Flytec6015.cpp
+++ src/database/flytec6015/Flytec6015.cpp
@@ -39,9 +39,6 @@ void Flytec6015::open()
 
 	success = m_protocol->open(IFlyHighRC::pInstance()->deviceLine(), IFlyHighRC::pInstance()->deviceSpeedString().toUInt());
 	Error::verify(success, Error::FLYTEC_OPEN);
-
-//writeFaUInt16(0x1a, 420);
-int value = readFaUInt16(0x1a);
 }
 
 void Flytec6015::close()
@@ -54,69 +51,14 @@ void Flytec6015::cancel()
 	m_cancel = true;
 }
 
-bool Flytec6015::writeEnableFa()
+bool Flytec6015::memoryWrite(MemType memType, int par, DataType dataType, const QVariant &value)
 {
-	return m_protocol->writeEnableFa();
+	return m_protocol->memoryWrite(memType, par, dataType, value);
 }
 
-bool Flytec6015::writeFaString(int par, const QString &value)
+QVariant Flytec6015::memoryRead(MemType memType, int par, DataType dataType)
 {
-	return m_protocol->writeFaString(par, value);
-}
-
-QString Flytec6015::readFaString(int par)
-{
-	return m_protocol->readFaString(par);
-}
-
-bool Flytec6015::writeFaInt8(int par, char value)
-{
-	return m_protocol->writeFaInt8(par, value);
-}
-
-char Flytec6015::readFaInt8(int par)
-{
-	return (char)m_protocol->readFaInt32(par);
-}
-
-bool Flytec6015::writeFaInt16(int par, int value)
-{
-	return m_protocol->writeFaInt16(par, value);
-}
-
-int Flytec6015::readFaInt16(int par)
-{
-	return m_protocol->readFaInt32(par);
-}
-
-bool Flytec6015::writeFaUInt16(int par, uint value)
-{
-	return m_protocol->writeFaInt16(par, (int)value);
-}
-
-uint Flytec6015::readFaUInt16(int par)
-{
-	return (uint)m_protocol->readFaInt32(par);
-}
-
-bool Flytec6015::writeFaInt32(int par, int value)
-{
-	return m_protocol->writeFaInt32(par, value);
-}
-
-int Flytec6015::readFaInt32(int par)
-{
-	return m_protocol->readFaInt32(par);
-}
-
-bool Flytec6015::writeFaUInt32(int par, uint value)
-{
-	return m_protocol->writeFaInt32(par, (int)value);
-}
-
-uint Flytec6015::readFaUInt32(int par)
-{
-	return (uint)m_protocol->readFaInt32(par);
+	return m_protocol->memoryRead(memType, par, dataType);
 }
 
 bool Flytec6015::flightList(Pilot &pilot, Flight::FlightListType &flightList)
