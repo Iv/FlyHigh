96f603e5b5d0a9159d65f87330e4d359114b779b
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2010-03-22 21:01:08 +0000

    ifdef for header protection fixed
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1000 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/configuration/flytec/IFlytecConfig.h
+++ src/configuration/flytec/IFlytecConfig.h
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#ifndef CnfFlytecInterf_h
+#ifndef IFlytecConfig_h
 #define IFlytecConfig_h
  
 class QWidget;
