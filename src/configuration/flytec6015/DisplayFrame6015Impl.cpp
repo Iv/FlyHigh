982fc3e14f6b7417655957782d9a29b46b9830fc
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2010-08-15 11:17:20 +0000

    write support of parameters
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@1034 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/configuration/flytec6015/DisplayFrame6015Impl.cpp
+++ src/configuration/flytec6015/DisplayFrame6015Impl.cpp
@@ -47,74 +47,28 @@ void DisplayFrame6015Impl::update(QByteArray &arr)
 
 	// Alti, Line 4, Alt2, Alt3
 	uiValue = pDev->memoryRead(MemFa, DIV_FLAGS, UInt16).toUInt();
-	comboBox_AltDisp->setCurrentItem((uiValue & MASK_ALT_DISP) >> 4);
-	comboBox_Line4->setCurrentItem((uiValue & MASK_LINE4_DISP) >> 5);
-	comboBox_Alt2Mode->setCurrentItem((uiValue & MASK_ALT2_MODE) >> 6);
-	comboBox_Alt3Mode->setCurrentItem((uiValue & MASK_ALT3_MODE) >> 9);
+	comboBox_AltDisp->setCurrentItem((uiValue & MASK_ALT_DISP) >> POS_ALT_DISP);
+	comboBox_Line4->setCurrentItem((uiValue & MASK_LINE4_DISP) >> POS_LINE4_DISP);
+	comboBox_Alt2Mode->setCurrentItem((uiValue & MASK_ALT2_MODE) >> POS_ALT2_MODE);
+	comboBox_Alt3Mode->setCurrentItem((uiValue & MASK_ALT3_MODE) >> POS_ALT3_MODE);
 }
 
 void DisplayFrame6015Impl::store(QByteArray &arr)
 {
-/*
-	int16_t i16value;
+	Flytec6015 *pDev;
+	uint uiValue;
 
-	// LCD Contrast
-	i16value = slider_Contrast->value();
-	arr[LCD_CONTRAST_POS] = (u_char)(i16value >> 8);
-	arr[LCD_CONTRAST_POS+1] = (u_char)(i16value & 0xFF);
-	
-	// User Field 0
-	arr[USERFIELD_0_POS] = comboBox_UserField_0_0->currentItem();
-	arr[USERFIELD_0_POS+1] = comboBox_UserField_0_1->currentItem();
-	arr[USERFIELD_0_POS+2] = comboBox_UserField_0_2->currentItem();
-	
-	// User Field 1
-	arr[USERFIELD_1_POS] = comboBox_UserField_1_0->currentItem();
-	arr[USERFIELD_1_POS+1] = comboBox_UserField_1_1->currentItem();
-	arr[USERFIELD_1_POS+2] = comboBox_UserField_1_2->currentItem();
+	pDev = static_cast<Flytec6015*>(IGPSDevice::pInstance());
 
-	// User Field 2
-	arr[USERFIELD_2_POS] = comboBox_UserField_2_0->currentItem();
-	arr[USERFIELD_2_POS+1] = comboBox_UserField_2_1->currentItem();
-	arr[USERFIELD_2_POS+2] = comboBox_UserField_2_2->currentItem();
-*/
+	// Alti, Line 4, Alt2, Alt3
+	uiValue = pDev->memoryRead(MemFa, DIV_FLAGS, UInt16).toUInt();
+	uiValue &= ~(MASK_ALT_DISP | MASK_LINE4_DISP | MASK_ALT2_MODE | MASK_ALT3_MODE);
+	uiValue |= (comboBox_AltDisp->currentItem() << POS_ALT_DISP);
+	uiValue |= (comboBox_Line4->currentItem() << POS_LINE4_DISP);
+	uiValue |= (comboBox_Alt2Mode->currentItem() << POS_ALT2_MODE);
+	uiValue |= (comboBox_Alt3Mode->currentItem() << POS_ALT3_MODE);
+	pDev->memoryWrite(MemFa, DIV_FLAGS, UInt16, uiValue);
 }
-
-void DisplayFrame6015Impl::insertItems( QComboBox * pItem )
-{
-	pItem->clear();
-	pItem->insertItem( tr( "Wind spd" ) );
-	pItem->insertItem( tr( "Time" ) );
-	pItem->insertItem( tr( "Fl.Time" ) );
-	pItem->insertItem( tr( "Gnd spd" ) );
-	pItem->insertItem( tr( "Speed diff" ) );
-	pItem->insertItem( tr( "Dist WP" ) );
-	pItem->insertItem( tr( "Bearing" ) );
-	pItem->insertItem( tr( "Track" ) );
-	pItem->insertItem( tr( "Temp" ) );
-	pItem->insertItem( tr( "Alt 2" ) );
-	pItem->insertItem( tr( "Alt 3" ) );
-	pItem->insertItem( tr( "QNH hPa" ) );
-	pItem->insertItem( tr( "L/D Gnd" ) );
-	pItem->insertItem( tr( "L/D air" ) );
-	pItem->insertItem( tr( "L/D Req" ) );
-	pItem->insertItem( tr( "Dist to ^" ) );
-	pItem->insertItem( QString::null );
-	pItem->insertItem( tr( "Alt @ BG" ) );
-	pItem->insertItem( tr( "FL (ft)" ) );
-	pItem->insertItem( tr( "Air spd" ) );
-	pItem->insertItem( tr( "Alt @ goal" ) );
-	pItem->insertItem( tr( "Dist goal" ) );
-	pItem->insertItem( tr( "Vario" ) );
-	pItem->insertItem( tr( "Alt 1" ) );
-	pItem->insertItem( tr( "SMS p/t" ) );
-	pItem->insertItem( tr( "Dist toff" ) );
-	pItem->insertItem( tr( "Dist cyl" ) );
-	pItem->insertItem( tr( "L/D rel goal" ) );
-	pItem->insertItem( tr( "Race start" ) );
-}
-
-
 
 #include "DisplayFrame6015Impl.moc"
 
