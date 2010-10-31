b8a8efa03f78b472cda1eea3ed929e520d1252bb
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2010-10-31 14:27:42 +0000

    correct email
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1136 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/preparation/WebMapRouteWindow.h
+++ src/preparation/WebMapRouteWindow.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2010 by Alex Graf   *
- *   alex@grafitation.ch   *
+ *   Copyright (C) 2010 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
