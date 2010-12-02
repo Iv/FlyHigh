241175010fe8da52872c4fb0ab94880b081c0ca0
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2010-12-02 20:10:07 +0000

    Reimplemented from analysis/WebMapView
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@1222 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ src/common/webmap/WebMapFlightView.cpp
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "Flight.h"
+#include "PolyLineEncoder.h"
+#include "WebMap.h"
+#include "WebMapFlightView.h"
+
+WebMapFlightView::WebMapFlightView(const QString &name)
+{
+	QWidget::setWindowTitle(name);
+	resize(1000, 800);
+
+	m_pWebMap = new WebMap(this);
+	m_pFlight = NULL;
+	connect(m_pWebMap, SIGNAL(mapReady()), this, SLOT(mapReady()));
+}
+
+WebMapFlightView::~WebMapFlightView()
+{
+	delete m_pWebMap;
+}
+
+void WebMapFlightView::setTurnPointList(const FlightPointList &tpList)
+{
+	m_tpList = tpList;
+}
+
+void WebMapFlightView::setWayPointList(const WayPoint::WayPointListType &wpList)
+{
+	m_wpList = wpList;
+}
+
+void WebMapFlightView::setFlight(const Flight *pFlight)
+{
+	m_pFlight = pFlight;
+}
+
+void WebMapFlightView::loadMap()
+{
+	m_pWebMap->loadMap("qrc:/webmap_analysis.html");
+}
+
+void WebMapFlightView::resizeEvent(QResizeEvent *pEvent)
+{
+	m_pWebMap->setGeometry(QRect(0, 0, width(), height()));
+}
+
+void WebMapFlightView::mapReady()
+{
+	PolyLineEncoder encoder;
+	QString encPoints;
+	QString encLevels;
+
+	m_pWebMap->setTurnPointList(m_tpList);
+	m_pWebMap->setTurnPointsDragable(false);
+	m_pWebMap->setFlightType("xc5");
+	m_pWebMap->XCLoad();
+	encoder.dpEncode(m_wpList, encPoints, encLevels);
+	m_pWebMap->setWayPointList(encPoints, encLevels, 3, "#FF0000");
+
+}
---
+++ src/common/webmap/WebMapFlightView.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef _WebMapFlightView_h
+#define _WebMapFlightView_h
+
+#include <QWidget>
+#include "FlightPointList.h"
+#include "WayPoint.h"
+
+class Flight;
+class FlightPointList;
+class WebMap;
+
+class WebMapFlightView: public QWidget
+{
+	Q_OBJECT
+
+	public:
+		WebMapFlightView(const QString &name);
+
+		~WebMapFlightView();
+
+		void setTurnPointList(const FlightPointList &tpList);
+
+		void setWayPointList(const WayPoint::WayPointListType &wpList);
+
+		void setFlight(const Flight *pFlight);
+
+		void loadMap();
+
+	protected:
+		void resizeEvent(QResizeEvent *pEvent);
+
+	private:
+		WebMap *m_pWebMap;
+		WayPoint::WayPointListType m_wpList;
+		FlightPointList m_tpList;
+		const Flight *m_pFlight;
+
+	public slots:
+		void mapReady();
+};
+
+#endif
