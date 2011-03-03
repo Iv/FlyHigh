5d12cc0ecea486153af35d6652f7e3db9597438a
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2011-03-03 20:42:51 +0000

    support for track list
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1396 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/database/flytec/Protocol5020.cpp
+++ src/database/flytec/Protocol5020.cpp
@@ -17,8 +17,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
- 
+
+#include "Flight.h"
 #include "Protocol5020.h"
+#include "Tokenizer.h"
 
 #include <QtDebug>
 
@@ -65,6 +67,137 @@ qDebug() << m_device.getTlg();
 	}
 }
 
+bool Protocol5020::trackListReq()
+{
+	QString tlg;
+	
+	tlg = "$PBRTL,";
+	addTail(tlg);
+	m_device.sendTlg(tlg);
+}
+
+bool Protocol5020::trackListRec(uint &total, Flight &flight)
+{
+	Tokenizer tokenizer;
+	QString token;
+	QString tlg;
+	bool valid = false;
+
+	if(m_device.recieveTlg(500))
+	{
+		tlg = m_device.getTlg();
+
+		tokenizer.getFirstToken(tlg, ',', token); // $PBRTL
+		valid = (token == "$PBRTL");
+		valid &= validateCheckSum(tlg);
+
+		if(valid)
+		{
+			// total
+			tokenizer.getNextToken(tlg, ',', token);
+			total = token.toUInt();
+
+			// nr
+			tokenizer.getNextToken(tlg, ',', token);
+			flight.setNumber(token.toUInt());
+
+			// date
+			tokenizer.getNextToken(tlg, ',', token);
+			flight.setDate(parseDate(token));
+
+			// time
+			tokenizer.getNextToken(tlg, ',', token);
+			flight.setTime(parseTime(token));
+
+			// duration
+			tokenizer.getNextToken(tlg, '*', token);
+			flight.setDuration(parseTime(token));
+		}
+	}
+
+	return valid;
+
+#if 0
+	res = flytec_ll_recieve(ProductResp, &buff[0], &len);
+	
+	if(res == 0)
+	{
+		res = memcmp((char*)&buff[0], "PBRTL", 5);
+	}
+	
+	if(res == 0)
+	{
+		/* nof tracks */
+		buff[8] = '\0';
+		pTrack->totalNum = atoi((char*)&buff[6]);
+		
+		/* track number */
+		buff[11] = '\0';
+		pTrack->trackNum = pTrack->totalNum - atoi((char*)&buff[9]) - 1;
+		
+		/* date */
+		buff[14] = '\0';
+		pTrack->date.day= atoi((char*)&buff[12]);
+		buff[17] = '\0';
+		pTrack->date.month= atoi((char*)&buff[15]);
+		buff[20] = '\0';
+		pTrack->date.year= 2000 + atoi((char*)&buff[18]);
+		
+		/* start */
+		buff[23] = '\0';
+		pTrack->start.hour = atoi((char*)&buff[21]);
+		buff[26] = '\0';
+		pTrack->start.min = atoi((char*)&buff[24]);
+		buff[29] = '\0';
+		pTrack->start.sec = atoi((char*)&buff[27]);
+		
+		/* duration */
+		buff[32] = '\0';
+		pTrack->duration.hour = atoi((char*)&buff[30]);
+		buff[35] = '\0';
+		pTrack->duration.min = atoi((char*)&buff[33]);
+		buff[38] = '\0';
+		pTrack->duration.sec = atoi((char*)&buff[36]);
+	}
+#endif
+}
+
+QDate Protocol5020::parseDate(const QString &token) const
+{
+	Tokenizer tokenizer;
+	QString timeToken;
+	int year;
+	int month;
+	int day;
+
+	tokenizer.getFirstToken(token, '.', timeToken);
+	day = timeToken.toInt();
+	tokenizer.getNextToken(token, '.', timeToken);
+	month = timeToken.toInt();
+	tokenizer.getNextToken(token, '\0', timeToken);
+	year = timeToken.toInt() + 2000;
+
+	return QDate(year, month, day);
+}
+
+QTime Protocol5020::parseTime(const QString &token) const
+{
+	Tokenizer tokenizer;
+	QString timeToken;
+	int hour;
+	int min;
+	int sec;
+
+	tokenizer.getFirstToken(token, ':', timeToken);
+	hour = timeToken.toInt();
+	tokenizer.getNextToken(token, ':', timeToken);
+	min = timeToken.toInt();
+	tokenizer.getNextToken(token, '\0', timeToken);
+	sec = timeToken.toInt();
+
+	return QTime(hour, min, sec);
+}
+
 void Protocol5020::addTail(QString &tlg) const
 {
 	QString checkSum;
@@ -89,4 +222,17 @@ QString Protocol5020::getCheckSum(const QString &tlg, uint end) const
 	snprintf(strCheckSum, CheckSumSize + 1, "%02X", checkSum);
 
 	return strCheckSum;
+}
+
+bool Protocol5020::validateCheckSum(const QString &tlg) const
+{
+	Tokenizer tokenizer;
+	QString token;
+	QString checkSum;
+
+	tokenizer.getFirstToken(tlg, '*', token);
+	tokenizer.getNextToken(tlg, '\r', token);
+	checkSum = getCheckSum(tlg, tlg.size() - 5);
+
+	return (checkSum == token);
 }
