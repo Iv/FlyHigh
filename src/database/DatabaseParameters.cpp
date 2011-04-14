6cc797b6aeb4e3397492168cabb8391975e67020
ja_kern <ja_kern@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2011-04-14 11:48:45 +0000

    Added class DatabaseParameter to project.
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1466 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ src/database/DatabaseParameters.cpp
@@ -0,0 +1,171 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QFileInfo>
+#include <QDir>
+#include "IFlyHighRC.h"
+#include "DatabaseParameters.h"
+
+DatabaseParameters::DatabaseParameters()
+{
+}
+
+DatabaseParameters::DatabaseParameters(const QString& type,
+                                       const QString& databaseName,
+                                       const QString& hostName,
+                                       int port,
+                                       const QString& userName,
+																			 const QString& password,
+																			 const QString& file)
+	: m_dBType(type)
+	, m_dBName(databaseName)
+	, m_hostName(hostName)
+	, m_port(port)
+	, m_userName(userName)
+	, m_password(password)
+	, m_dBFile(file)
+{
+	validateDBType();
+	validateDBFile();
+}
+
+bool DatabaseParameters::operator==(const DatabaseParameters& other) const
+{
+	return m_dBType   == other.m_dBType &&
+			m_dBName   == other.m_dBName &&
+			m_hostName == other.m_hostName &&
+			m_port     == other.m_port &&
+			m_userName == other.m_userName &&
+			m_password == other.m_password &&
+			m_dBFile   == other.m_dBFile;
+}
+
+bool DatabaseParameters::operator!=(const DatabaseParameters& other) const
+{
+	return !operator==(other);
+}
+
+bool DatabaseParameters::isSQLite() const
+{
+	return m_dBType == DatabaseParameters::SQLiteDatabaseType();
+}
+
+bool DatabaseParameters::isMySQL() const
+{
+	return m_dBType == DatabaseParameters::MySQLDatabaseType();
+}
+
+QString DatabaseParameters::SQLiteDatabaseType()
+{
+	return "QSQLITE";
+}
+
+QString DatabaseParameters::MySQLDatabaseType()
+{
+	return "QMYSQL";
+}
+
+void DatabaseParameters::readFromConfig()
+{
+	m_dBType = IFlyHighRC::pInstance()->dBType();
+	m_dBName = IFlyHighRC::pInstance()->dBName();
+	m_hostName = IFlyHighRC::pInstance()->dBHost();
+	m_port = IFlyHighRC::pInstance()->dBPort();
+	m_userName = IFlyHighRC::pInstance()->dBUser();
+	m_password = IFlyHighRC::pInstance()->dBPass();
+	m_dBFile = IFlyHighRC::pInstance()->dBFile();
+
+	validateDBType();
+	validateDBFile();
+}
+
+DatabaseParameters DatabaseParameters::defaultParameters()
+{
+	DatabaseParameters parameters;
+
+	parameters.m_dBType = "QSQLITE";
+	parameters.m_dBName = "flyhigh_v2";
+	parameters.m_hostName = "localhost";
+	parameters.m_userName = "flyhigh";
+	parameters.m_password = "flyhigh";
+	parameters.m_port = 3306;
+	parameters.m_dBFile = "Flights/flyhigh_v2.sqlite";
+
+	parameters.validateDBFile();
+
+	return parameters;
+}
+
+void DatabaseParameters::validateDBType()
+{
+	if (m_dBType.contains("sqlite",Qt::CaseInsensitive))
+	{
+		m_dBType = DatabaseParameters::SQLiteDatabaseType();
+	}
+	else
+	{
+		m_dBType = DatabaseParameters::MySQLDatabaseType();
+	}
+}
+
+void DatabaseParameters::validateDBFile()
+{
+	if (QFileInfo(m_dBFile).isRelative())
+	{
+		// prepend userhome
+		m_dBFile.prepend(QDir::separator()).prepend(QDir::homePath());
+	}
+}
+
+const QString& DatabaseParameters::dBType() const
+{
+	return m_dBType;
+}
+
+const QString& DatabaseParameters::dBName() const
+{
+	return m_dBName;
+}
+
+const QString& DatabaseParameters::dBHostName() const
+{
+	return m_hostName;
+}
+
+const QString& DatabaseParameters::dBUserName() const
+{
+	return m_userName;
+}
+
+const QString& DatabaseParameters::dBPassword() const
+{
+	return m_password;
+}
+
+const QString& DatabaseParameters::dBFile() const
+{
+	return m_dBFile;
+}
+
+int DatabaseParameters::dBPort() const
+{
+	return m_port;
+}
+
---
+++ src/database/DatabaseParameters.h
@@ -0,0 +1,81 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef DATABASEPARAMETERS_H
+#define DATABASEPARAMETERS_H
+
+#include <QString>
+
+/**
+ * Encapsulates database parameters.
+ */
+class DatabaseParameters
+{
+
+public:
+
+	DatabaseParameters(const QString& type,
+										 const QString& name,
+										 const QString& hostName,
+										 int   port,
+										 const QString& user,
+										 const QString& password,
+										 const QString& file);
+
+	DatabaseParameters();
+
+	bool operator==(const DatabaseParameters& other) const;
+	bool operator!=(const DatabaseParameters& other) const;
+
+	bool isSQLite() const;
+	bool isMySQL() const;
+
+	void readFromConfig();
+
+	const QString& dBType() const;
+	const QString& dBName() const;
+	const QString& dBHostName() const;
+	const QString& dBUserName() const;
+	const QString& dBPassword() const;
+	const QString& dBFile() const;
+	int dBPort() const;
+
+	static QString SQLiteDatabaseType();
+	static QString MySQLDatabaseType();
+	static DatabaseParameters defaultParameters();
+	static DatabaseParameters parametersFromConfig();
+
+private:
+	void validateDBType();
+	void validateDBFile();
+
+private:
+
+	QString m_dBType;
+	QString m_dBName;
+	QString m_hostName;
+	int     m_port;
+	QString m_userName;
+	QString m_password;
+	QString m_dBFile;
+
+};
+
+#endif
