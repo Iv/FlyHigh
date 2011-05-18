97ea3699edd671f6d9366d209cbc52dfaeacc540
ja_kern <ja_kern@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2011-05-18 19:25:22 +0000

    Now using query store for db table creation
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@1475 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/database/sql/QueryStore.cpp
+++ src/database/sql/QueryStore.cpp
@@ -103,6 +103,196 @@ void QueryStore::init()
 	m_QueryMap.insert("migrate-drop-routeitems",
 										"DROP TABLE IF EXISTS RouteItems");
 
+
+	m_QueryMapMysql.insert("setup-create-gliders",
+												 "CREATE TABLE `Gliders`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`Manufacturer` VARCHAR(16) CHARACTER SET utf8 NULL DEFAULT '',"
+												 "`Model` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
+												 "`Serial` VARCHAR(16) CHARACTER SET utf8 NULL DEFAULT '',"
+												 "PRIMARY KEY(`Id`),"
+												 "UNIQUE KEY(`Manufacturer`, `Model`, `Serial`)"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-gliders",
+													"CREATE TABLE `Gliders`"
+													"("
+													"`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+													"`Manufacturer` VARCHAR(16) DEFAULT '',"
+													"`Model` VARCHAR(16) NOT NULL,"
+													"`Serial` VARCHAR(16) DEFAULT '')");
+
+	m_QueryMapMysql.insert("setup-create-pilots",
+												 "CREATE TABLE `Pilots`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`FirstName` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
+												 "`LastName` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
+												 "`BirthDate` DATE NULL DEFAULT '0000-00-00',"
+												 "`CallSign` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
+												 "`GliderId` INT NULL DEFAULT '0',"
+												 "PRIMARY KEY(`Id`),"
+												 "UNIQUE KEY byPilot(`FirstName`, `LastName`, `BirthDate`),"
+												 "FOREIGN KEY(`GliderId`) REFERENCES Gliders(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-pilots",
+													"CREATE TABLE `Pilots`"
+													"("
+													"`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+													"`FirstName` VARCHAR(16) NOT NULL,"
+													"`LastName` VARCHAR(16) NOT NULL,"
+													"`BirthDate` DATE DEFAULT '0000-00-00',"
+													"`CallSign` VARCHAR(16) NOT NULL,"
+													"`GliderId` INTEGER DEFAULT '0',"
+													"CONSTRAINT Pilots_fk1 FOREIGN KEY (`GliderId`)"
+													" REFERENCES `Gliders` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)");
+
+	m_QueryMapMysql.insert("setup-create-waypoints",
+												 "CREATE TABLE `WayPoints`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`Name` VARCHAR(16) NOT NULL,"
+												 "`Spot` VARCHAR(16) NULL DEFAULT '',"
+												 "`Country` VARCHAR(2) NULL DEFAULT '',"
+												 "`Longitude` FLOAT NOT NULL,"
+												 "`Latitude` FLOAT NOT NULL,"
+												 "`Altitude` INT NOT NULL,"
+												 "`Description` VARCHAR(200) NULL DEFAULT '',"
+												 "PRIMARY KEY(`Id`),"
+												 "UNIQUE KEY byWayPoints(`Name`, `Spot`, `Country`)"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-waypoints",
+													"CREATE TABLE `WayPoints`"
+													"("
+													"`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+													"`Name` VARCHAR(16) NOT NULL,"
+													"`Spot` VARCHAR(16) DEFAULT '',"
+													"`Country` VARCHAR(2) DEFAULT '',"
+													"`Longitude` FLOAT NOT NULL,"
+													"`Latitude` FLOAT NOT NULL,"
+													"`Altitude` INTEGER NOT NULL,"
+													"`Description` VARCHAR(200) DEFAULT '')");
+
+	m_QueryMapMysql.insert("setup-create-flights",
+												 "CREATE TABLE `Flights`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`Number` INT NOT NULL,"
+												 "`PilotId` INT NOT NULL,"
+												 "`Date` DATE NOT NULL,"
+												 "`Time` TIME NOT NULL,"
+												 "`GliderId` INT NOT NULL,"
+												 "`StartPtId` INT NOT NULL,"
+												 "`LandPtId` INT NOT NULL,"
+												 "`Duration` INT NOT NULL,"
+												 "`Distance` INT NOT NULL,"
+												 "`Comment` VARCHAR(500) NULL DEFAULT '',"
+												 "`IGCFile` MEDIUMBLOB,"
+												 "PRIMARY KEY(`Id`),"
+												 "UNIQUE KEY byFlight(`PilotId`, `Number`),"
+												 "FOREIGN KEY(`PilotId`) REFERENCES Pilots(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+												 "FOREIGN KEY(`GliderId`) REFERENCES Gliders(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+												 "FOREIGN KEY(`StartPtId`) REFERENCES WayPoints(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+												 "FOREIGN KEY(`LandPtId`) REFERENCES WayPoints(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-flights",
+													"CREATE TABLE `Flights`"
+													"("
+													"`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+													"`Number` INTEGER NOT NULL,"
+													"`PilotId` INTEGER NOT NULL,"
+													"`Date` DATE NOT NULL,"
+													"`Time` TIME NOT NULL,"
+													"`GliderId` INTEGER NOT NULL,"
+													"`StartPtId` INTEGER NOT NULL,"
+													"`LandPtId` INTEGER NOT NULL,"
+													"`Duration` INTEGER NOT NULL,"
+													"`Distance` INTEGER NOT NULL,"
+													"`Comment` VARCHAR(500) DEFAULT '',"
+													"`IGCFile` MEDIUMBLOB,"
+													"CONSTRAINT Flights_fk1 FOREIGN KEY (`PilotId`)"
+													" REFERENCES `Pilots` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+													"CONSTRAINT Flights_fk2 FOREIGN KEY (`GliderId`)"
+													" REFERENCES `Gliders` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+													"CONSTRAINT Flights_fk3 FOREIGN KEY (`StartPtId`)"
+													" REFERENCES `WayPoints` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+													"CONSTRAINT Flights_fk4 FOREIGN KEY (`LandPtId`)"
+													" REFERENCES `WayPoints` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)");
+
+	m_QueryMapMysql.insert("setup-create-routes",
+												 "CREATE TABLE `Routes`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`Name` VARCHAR(16) NOT NULL,"
+												 "`Type` INT NULL DEFAULT 0,"
+												 "PRIMARY KEY(`Id`),"
+												 "UNIQUE KEY byRoutes(`Name`)"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-routes",
+													"CREATE TABLE `Routes`"
+													"("
+													"`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+													"`Name` VARCHAR(16) NOT NULL,"
+													"`Type` INTEGER DEFAULT 0)");
+
+	m_QueryMapMysql.insert("setup-create-routeitems",
+												 "CREATE TABLE `RouteItems`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`RouteId` INT NOT NULL,"
+												 "`WayPointId` INT NOT NULL,"
+												 "PRIMARY KEY(`Id`),"
+												 "FOREIGN KEY(`RouteId`) REFERENCES Routes(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+												 "FOREIGN KEY(`WayPointId`) REFERENCES WayPoints(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-routeitems",
+													"CREATE TABLE `RouteItems`"
+													"("
+													"`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+													"`RouteId` INTEGER NOT NULL,"
+													"`WayPointId` INTEGER NOT NULL,"
+													"CONSTRAINT RouteItems_fk1 FOREIGN KEY (`RouteId`)"
+													" REFERENCES `Routes` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
+													"CONSTRAINT RouteItems_fk2 FOREIGN KEY (`WayPointId`)"
+													" REFERENCES `WayPoints` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)");
+
+	m_QueryMapMysql.insert("setup-create-servicings",
+												 "CREATE TABLE `Servicings`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`GliderId` INT NOT NULL,"
+												 "`Date` DATE NOT NULL,"
+												 "`Responsibility` VARCHAR(16) NOT NULL,"
+												 "`Comment` VARCHAR(200) NULL DEFAULT '',"
+												 "PRIMARY KEY(`Id`),"
+												 "FOREIGN KEY(`GliderId`) REFERENCES Gliders(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-servicings",
+													"CREATE TABLE `Servicings`"
+													"("
+													"`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+													"`GliderId` INTEGER NOT NULL,"
+													"`Date` DATE NOT NULL,"
+													"`Responsibility` VARCHAR(16) NOT NULL,"
+													"`Comment` VARCHAR(200) DEFAULT '',"
+													"CONSTRAINT Servicings_fk1 FOREIGN KEY (`GliderId`)"
+													" REFERENCES `Gliders` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)");
+
+	m_QueryMapMysql.insert("setup-create-lastmodified",
+												 "CREATE TABLE `LastModified`"
+												 "("
+												 "`Id` INT NULL AUTO_INCREMENT,"
+												 "`Name` VARCHAR(16) NOT NULL,"
+												 "`Time` datetime NOT NULL,"
+												 "PRIMARY KEY(`Id`),"
+												 "UNIQUE KEY (`Name`)"
+												 ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+	m_QueryMapSqlite.insert("setup-create-lastmodified",
+													"CREATE TABLE `LastModified`"
+													"("
+													"`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+													"`Name` VARCHAR(16) NOT NULL,"
+													"`Time` datetime NOT NULL)");
 }
 
 QString QueryStore::getQuery(const QString& name, const QSqlDatabase& db) const
--- src/database/sql/Upgrade.cpp
+++ src/database/sql/Upgrade.cpp
@@ -22,15 +22,16 @@
 #include <qsqldatabase.h>
 #include <qsqlquery.h>
 #include <stdlib.h>
+#include "QueryStore.h"
 #include "Error.h"
 #include "Upgrade.h"
 
-const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_3_0 = QDateTime(QDate(2004, 3, 20), QTime(20, 0));
-const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_3_1 = QDateTime(QDate(2005, 6, 12), QTime(20, 0));
-const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_3_2 = QDateTime(QDate(2006, 3, 31), QTime(20, 50));
-const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_5_0 = QDateTime(QDate(2006, 11, 14), QTime(0, 0));
-const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_8_1 = QDateTime(QDate(2011, 1, 18), QTime(0, 0));
-const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_8_2 = QDateTime(QDate(2011, 2, 20), QTime(0, 0));
+const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_3_0 = QDateTime(QDate(2004,  3, 20), QTime(20, 0));
+const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_3_1 = QDateTime(QDate(2005,  6, 12), QTime(20, 0));
+const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_3_2 = QDateTime(QDate(2006,  3, 31), QTime(20,50));
+const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_5_0 = QDateTime(QDate(2006, 11, 14), QTime( 0, 0));
+const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_8_1 = QDateTime(QDate(2011,  1, 18), QTime( 0, 0));
+const Upgrade::DataBaseVersion Upgrade::DataBaseVersion_0_8_2 = QDateTime(QDate(2011,  2, 20), QTime( 0, 0));
 
 Upgrade::Upgrade(QSqlDatabase DB)
 	:DataBaseSub(DB)
@@ -42,7 +43,9 @@ bool Upgrade::setup(const QString &dbname, const QString &user, const QString &p
 	QSqlQuery query(db());
 	QString sqls;
 	bool res = true;
+	QueryStore* pQueryStore = QueryStore::pInstance();
 
+	// prepare db
   if (db().driverName()=="QMYSQL")
   {
     // setup mysql db
@@ -57,245 +60,67 @@ bool Upgrade::setup(const QString &dbname, const QString &user, const QString &p
 
     sqls = "USE `%1`;";
 		res &= query.exec(sqls.arg(dbname));
+	}
+	else if (db().driverName()=="QSQLITE")
+	{
+		// setup sqlite db
 
-    sqls = "CREATE TABLE `Gliders`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`Manufacturer` VARCHAR(16) CHARACTER SET utf8 NULL DEFAULT '',"
-           "`Model` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
-           "`Serial` VARCHAR(16) CHARACTER SET utf8 NULL DEFAULT '',"
-           "PRIMARY KEY(`Id`),"
-           "UNIQUE KEY(`Manufacturer`, `Model`, `Serial`)"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+		// this replaces the 'CHARSET=utf8' clause in mysql
+		sqls = "PRAGMA encoding = 'UTF-8'";
 		res &= query.exec(sqls);
+	}
+	else
+	{
+		return false;
+	}
 
-    sqls = "CREATE TABLE `Pilots`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`FirstName` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
-           "`LastName` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
-           "`BirthDate` DATE NULL DEFAULT '0000-00-00',"
-           "`CallSign` VARCHAR(16) CHARACTER SET utf8 NOT NULL,"
-           "`GliderId` INT NULL DEFAULT '0',"
-           "PRIMARY KEY(`Id`),"
-           "UNIQUE KEY byPilot(`FirstName`, `LastName`, `BirthDate`),"
-           "FOREIGN KEY(`GliderId`) REFERENCES Gliders(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
-		res &= query.exec(sqls);
+	// create tables
+	res &= query.exec(pQueryStore->getQuery("setup-create-gliders", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-pilots", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-waypoints", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-flights", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-routes", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-routeitems", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-servicings", db()));
+	res &= query.exec(pQueryStore->getQuery("setup-create-lastmodified", db()));
 
-    sqls = "CREATE TABLE `WayPoints`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`Name` VARCHAR(16) NOT NULL,"
-           "`Spot` VARCHAR(16) NULL DEFAULT '',"
-           "`Country` VARCHAR(2) NULL DEFAULT '',"
-           "`Longitude` FLOAT NOT NULL,"
-           "`Latitude` FLOAT NOT NULL,"
-           "`Altitude` INT NOT NULL,"
-           "`Description` VARCHAR(200) NULL DEFAULT '',"
-           "PRIMARY KEY(`Id`),"
-           "UNIQUE KEY byWayPoints(`Name`, `Spot`, `Country`)"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+	// finish db
+	if (db().driverName()=="QMYSQL")
+	{
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(1, 'Pilots', '1970-01-01 01:01:00'),"
+				"(2, 'Flights', '1970-01-01 01:01:00'),"
+				"(3, 'Gliders', '1970-01-01 01:01:00'),"
+				"(4, 'Servicings', '1970-01-01 01:01:00'),"
+				"(5, 'WayPoints', '1970-01-01 01:01:00'),"
+				"(6, 'DataBaseVersion', '2011-01-18 00:00:00'),"
+				"(7, 'Routes', '1970-01-01 01:01:00');";
 		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Flights`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`Number` INT NOT NULL,"
-           "`PilotId` INT NOT NULL,"
-           "`Date` DATE NOT NULL,"
-           "`Time` TIME NOT NULL,"
-           "`GliderId` INT NOT NULL,"
-           "`StartPtId` INT NOT NULL,"
-           "`LandPtId` INT NOT NULL,"
-           "`Duration` INT NOT NULL,"
-           "`Distance` INT NOT NULL,"
-           "`Comment` VARCHAR(500) NULL DEFAULT '',"
-           "`IGCFile` MEDIUMBLOB,"
-           "PRIMARY KEY(`Id`),"
-           "UNIQUE KEY byFlight(`PilotId`, `Number`),"
-           "FOREIGN KEY(`PilotId`) REFERENCES Pilots(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "FOREIGN KEY(`GliderId`) REFERENCES Gliders(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "FOREIGN KEY(`StartPtId`) REFERENCES WayPoints(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "FOREIGN KEY(`LandPtId`) REFERENCES WayPoints(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+	}
+	else if (db().driverName()=="QSQLITE")
+	{
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(1, 'Pilots', '1970-01-01 01:01:00')";
 		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Routes`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`Name` VARCHAR(16) NOT NULL,"
-           "`Type` INT NULL DEFAULT 0,"
-           "PRIMARY KEY(`Id`),"
-           "UNIQUE KEY byRoutes(`Name`)"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(2, 'Flights', '1970-01-01 01:01:00')";
 		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `RouteItems`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`RouteId` INT NOT NULL,"
-           "`WayPointId` INT NOT NULL,"
-           "PRIMARY KEY(`Id`),"
-           "FOREIGN KEY(`RouteId`) REFERENCES Routes(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "FOREIGN KEY(`WayPointId`) REFERENCES WayPoints(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(3, 'Gliders', '1970-01-01 01:01:00')";
 		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Servicings`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`GliderId` INT NOT NULL,"
-           "`Date` DATE NOT NULL,"
-           "`Responsibility` VARCHAR(16) NOT NULL,"
-           "`Comment` VARCHAR(200) NULL DEFAULT '',"
-           "PRIMARY KEY(`Id`),"
-           "FOREIGN KEY(`GliderId`) REFERENCES Gliders(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(4, 'Servicings', '1970-01-01 01:01:00')";
 		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `LastModified`"
-           "("
-           "`Id` INT NULL AUTO_INCREMENT,"
-           "`Name` VARCHAR(16) NOT NULL,"
-           "`Time` datetime NOT NULL,"
-           "PRIMARY KEY(`Id`),"
-           "UNIQUE KEY (`Name`)"
-           ")ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(5, 'WayPoints', '1970-01-01 01:01:00')";
 		res &= query.exec(sqls);
-
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(1, 'Pilots', '1970-01-01 01:01:00'),"
-           "(2, 'Flights', '1970-01-01 01:01:00'),"
-           "(3, 'Gliders', '1970-01-01 01:01:00'),"
-           "(4, 'Servicings', '1970-01-01 01:01:00'),"
-           "(5, 'WayPoints', '1970-01-01 01:01:00'),"
-           "(6, 'DataBaseVersion', '2011-01-18 00:00:00'),"
-           "(7, 'Routes', '1970-01-01 01:01:00');";
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(6, 'DataBaseVersion', '2011-01-18 00:00:00')";
 		res &= query.exec(sqls);
-  }
-  else if (db().driverName()=="QSQLITE")
-  {
-    // setup sqlite db
-
-    // this replaces the 'CHARSET=utf8' clause in mysql
-    sqls = "PRAGMA encoding = 'UTF-8'";
+		sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
+				"(7, 'Routes', '1970-01-01 01:01:00')";
 		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Gliders`"
-           "("
-           "`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-           "`Manufacturer` VARCHAR(16) DEFAULT '',"
-           "`Model` VARCHAR(16) NOT NULL,"
-           "`Serial` VARCHAR(16) DEFAULT '')";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Pilots`"
-           "("
-           "`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-           "`FirstName` VARCHAR(16) NOT NULL,"
-           "`LastName` VARCHAR(16) NOT NULL,"
-           "`BirthDate` DATE DEFAULT '0000-00-00',"
-           "`CallSign` VARCHAR(16) NOT NULL,"
-           "`GliderId` INTEGER DEFAULT '0',"
-           "CONSTRAINT Pilots_fk1 FOREIGN KEY (`GliderId`)"
-           " REFERENCES `Gliders` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `WayPoints`"
-           "("
-           "`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-           "`Name` VARCHAR(16) NOT NULL,"
-           "`Spot` VARCHAR(16) DEFAULT '',"
-           "`Country` VARCHAR(2) DEFAULT '',"
-           "`Longitude` FLOAT NOT NULL,"
-           "`Latitude` FLOAT NOT NULL,"
-           "`Altitude` INTEGER NOT NULL,"
-           "`Description` VARCHAR(200) DEFAULT '')";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Flights`"
-           "("
-           "`Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-           "`Number` INTEGER NOT NULL,"
-           "`PilotId` INTEGER NOT NULL,"
-           "`Date` DATE NOT NULL,"
-           "`Time` TIME NOT NULL,"
-           "`GliderId` INTEGER NOT NULL,"
-           "`StartPtId` INTEGER NOT NULL,"
-           "`LandPtId` INTEGER NOT NULL,"
-           "`Duration` INTEGER NOT NULL,"
-           "`Distance` INTEGER NOT NULL,"
-           "`Comment` VARCHAR(500) DEFAULT '',"
-           "`IGCFile` MEDIUMBLOB,"
-           "CONSTRAINT Flights_fk1 FOREIGN KEY (`PilotId`)"
-           " REFERENCES `Pilots` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "CONSTRAINT Flights_fk2 FOREIGN KEY (`GliderId`)"
-           " REFERENCES `Gliders` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "CONSTRAINT Flights_fk3 FOREIGN KEY (`StartPtId`)"
-           " REFERENCES `WayPoints` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "CONSTRAINT Flights_fk4 FOREIGN KEY (`LandPtId`)"
-           " REFERENCES `WayPoints` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Routes`"
-           "("
-           "`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-           "`Name` VARCHAR(16) NOT NULL,"
-           "`Type` INTEGER DEFAULT 0)";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `RouteItems`"
-           "("
-           "`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-           "`RouteId` INTEGER NOT NULL,"
-           "`WayPointId` INTEGER NOT NULL,"
-           "CONSTRAINT RouteItems_fk1 FOREIGN KEY (`RouteId`)"
-           " REFERENCES `Routes` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE,"
-           "CONSTRAINT RouteItems_fk2 FOREIGN KEY (`WayPointId`)"
-           " REFERENCES `WayPoints` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `Servicings`"
-           "("
-           "`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-           "`GliderId` INTEGER NOT NULL,"
-           "`Date` DATE NOT NULL,"
-           "`Responsibility` VARCHAR(16) NOT NULL,"
-           "`Comment` VARCHAR(200) DEFAULT '',"
-           "CONSTRAINT Servicings_fk1 FOREIGN KEY (`GliderId`)"
-           " REFERENCES `Gliders` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE)";
-		res &= query.exec(sqls);
-
-    sqls = "CREATE TABLE `LastModified`"
-           "("
-           "`Id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-           "`Name` VARCHAR(16) NOT NULL,"
-           "`Time` datetime NOT NULL)";
-		res &= query.exec(sqls);
-
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(1, 'Pilots', '1970-01-01 01:01:00')";
-		res &= query.exec(sqls);
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(2, 'Flights', '1970-01-01 01:01:00')";
-		res &= query.exec(sqls);
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(3, 'Gliders', '1970-01-01 01:01:00')";
-		res &= query.exec(sqls);
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(4, 'Servicings', '1970-01-01 01:01:00')";
-		res &= query.exec(sqls);
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(5, 'WayPoints', '1970-01-01 01:01:00')";
-		res &= query.exec(sqls);
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(6, 'DataBaseVersion', '2011-01-18 00:00:00')";
-		res &= query.exec(sqls);
-    sqls = "INSERT INTO `LastModified` (`Id`, `Name`, `Time`) VALUES"
-           "(7, 'Routes', '1970-01-01 01:01:00')";
-		res &= query.exec(sqls);
-  }
+	}
 	return res;
 }
 
