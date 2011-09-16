94e84f63017016aed3d7e2b4b762596527d20e42
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2011-09-16 15:07:47 +0000

    initial
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1580 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ src/common/webmap/WebMapFlight.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef WebMapFlight_h
+#define WebMapFlight_h
+
+#include <QObject>
+#include "FlightPointList.h"
+
+class WebMap;
+
+class WebMapFlight: public QObject
+{
+	Q_OBJECT
+
+	public:
+		WebMapFlight(WebMap *pWebMap);
+
+		~WebMapFlight();
+
+    void setFlightPointList(const FlightPointList &fpList);
+
+		void setSogList(const FlightPointList::SogListType &sogList);
+
+		void setVarioList(const FlightPointList::VarioListType &varioList);
+
+		void showPlot();
+
+		void setPlotEnable(bool en);
+
+	private:
+		WebMap *m_pWebMap;
+		uint m_plotEn;
+
+    void setWayPointList(const QString &encPoints, const QString &encLevels,
+                         uint weight, const QString &color);
+
+	private slots:
+		void populateJavaScriptWindowObject();
+};
+
+#endif
---
+++ src/common/webmap/WebMapRoute.cpp
@@ -0,0 +1,168 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QObject>
+#include <QWebFrame>
+#include "WebMap.h"
+#include "WebMapRoute.h"
+
+#include <QDebug>
+
+WebMapRoute::WebMapRoute(WebMap *pWebMap)
+{
+	QWebFrame *pFrame;
+
+	m_pWebMap = pWebMap;
+	pFrame = m_pWebMap->page()->mainFrame();
+	QObject::connect(pFrame, SIGNAL(javaScriptWindowObjectCleared()), this,
+                   SLOT(populateJavaScriptWindowObject()));
+}
+
+WebMapRoute::~WebMapRoute()
+{
+}
+
+void WebMapRoute::setName(const QString &name)
+{
+	QString code = "setName('%1');";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+	pFrame->evaluateJavaScript(code.arg(name));
+}
+
+QString WebMapRoute::getName()
+{
+	QString code = "getName();";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+
+	return pFrame->evaluateJavaScript(code).toString();
+}
+
+void WebMapRoute::setTurnPointList(const WayPoint::WayPointListType &tpList)
+{
+	QString code = "setTurnPointList('%1');";
+	QString arg = "[";
+	QString argElem = "[%1,%2]";
+	QWebFrame *pFrame;
+	int tpNr;
+	bool first = true;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+
+	for(tpNr=0; tpNr<tpList.size(); tpNr++)
+	{
+		if(!first)
+		{
+			arg += ",";
+		}
+
+		first = false;
+		arg += argElem.arg(tpList.at(tpNr).latitude()).arg(tpList.at(tpNr).longitude());
+	}
+
+	arg += "]";
+	pFrame->evaluateJavaScript(code.arg(arg));
+}
+
+void WebMapRoute::getTurnPointList(WayPoint::WayPointListType &tpList)
+{
+	QString code = "getTurnPointList();";
+	QWebFrame *pFrame;
+	QVariantList list;
+	QVariantList::iterator it;
+	QVariantList::iterator latLngIt;
+	WayPoint wp;
+	QString name;
+	int wpNr = 0;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+	list = pFrame->evaluateJavaScript(code).toList();
+	tpList.clear();
+
+	name = getName();
+	name += "_%1";
+
+	for(it=list.begin(); it!=list.end(); it++)
+	{
+		if((*it).toList().size() == 2)
+		{
+			wp.setName(name.arg(wpNr));
+			wp.setLatitude((*it).toList().at(0).toDouble());
+			wp.setLongitude((*it).toList().at(1).toDouble());
+			wpNr++;
+			tpList.push_back(wp);
+		}
+	}
+}
+
+void WebMapRoute::setTurnPointsDragable(bool en)
+{
+	QString code = "setTurnPointsDragable(%1);";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+	pFrame->evaluateJavaScript(code.arg(en));
+}
+
+void WebMapRoute::setLocation(const QString &loc)
+{
+	QString code = "setLocation('%1');";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+	pFrame->evaluateJavaScript(code.arg(loc));
+}
+
+QString WebMapRoute::getLocation()
+{
+	QString code = "getLocation();";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+
+	return pFrame->evaluateJavaScript(code).toString();
+}
+
+void WebMapRoute::setFlightType(const QString &flightType)
+{
+	QString code = "setFlightType('%1');";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+	pFrame->evaluateJavaScript(code.arg(flightType));
+}
+
+QString WebMapRoute::getFlightType() const
+{
+	QString code = "getFlightType();";
+	QWebFrame *pFrame;
+
+	pFrame = m_pWebMap->page()->mainFrame();
+
+	return pFrame->evaluateJavaScript(code).toString();
+}
+
+void WebMapRoute::populateJavaScriptWindowObject()
+{
+	m_pWebMap->page()->mainFrame()->addToJavaScriptWindowObject("WebMapRoute", this);
+}
