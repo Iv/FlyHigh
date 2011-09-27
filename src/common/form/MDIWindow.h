f4944d7ece76c3cece538ef8f5980672e7903fd7
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2011-09-27 20:28:58 +0000

    data refresh events moved to database
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1615 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/form/MDIWindow.h
+++ src/common/form/MDIWindow.h
@@ -1,8 +1,22 @@
-/* Copyright (c):  2004 by Alex Graf
-	This file is distributed under the terms of the General Public
-	Licence. See the file gpl.txt for the Licence or visit gnu.org 
-	for more information.
-*/
+/***************************************************************************
+ *   Copyright (C) 2004 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #ifndef _MDIWindow_h_
 #define _MDIWindow_h_
@@ -15,23 +29,12 @@ class QString;
 class MDIWindow: public QMainWindow
 {
 	Q_OBJECT
+
 	public:
 		MDIWindow(QWidget* parent, const char* name, Qt::WindowFlags wflags);
-		
+
 	signals:
 		void message(const QString&, int);
-
-		/**
-		 * Emit this signal if underlying data has changed
-		 */
-		void dataChanged();
-		
-	protected slots:
-		/**
-		 * Refresh displayed data.
-		 * Called when a dataChanged() signal was received
-		 */
-		virtual void refresh()=0;
 };
 
 #endif
