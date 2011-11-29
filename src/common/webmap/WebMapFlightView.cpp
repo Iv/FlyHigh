7a083f5bdfb1508c3584479d54dde1fc5e113702
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2011-11-29 19:39:07 +0000

    setFlightType() with enum
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1680 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/webmap/WebMapFlightView.cpp
+++ src/common/webmap/WebMapFlightView.cpp
@@ -83,7 +83,7 @@ void WebMapFlightView::resizeEvent(QResizeEvent *pEvent)
 void WebMapFlightView::mapReady()
 {
 	m_pWebMap->getRoute()->setTurnPointList(m_tpList);
-	m_pWebMap->getRoute()->setFlightType("xc5");
+	m_pWebMap->getRoute()->setFlightType(Route::Free3Tp);
 	m_pWebMap->getRoute()->setLocation(m_location);
 
 	m_pWebMap->getFlight()->init();
