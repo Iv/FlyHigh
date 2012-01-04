2f8587e69924edfe061a316c91b7bdb716b6d2ad
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2012-01-04 20:32:16 +0000

    added addAction() and actionList()
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@1735 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/form/MDIWindow.cpp
+++ src/common/form/MDIWindow.cpp
@@ -1,8 +1,22 @@
-/* Copyright (c):  2004 by Alex Graf
-	This file is distributed under the terms of the General Public
-	Licence. See the file gpl.txt for the Licence or visit gnu.org 
-	for more information.
-*/
+/***************************************************************************
+ *   Copyright (C) 2004 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include <QWidget>
 #include <QString>
@@ -13,4 +27,14 @@ MDIWindow::MDIWindow(QWidget* parent, const char* name, Qt::WindowFlags wflags)
 	:QMainWindow(parent, wflags)
 {
 	setWindowTitle(name);
+}
+
+void MDIWindow::addAction(QAction *pAction)
+{
+  m_actionList.push_back(pAction);
+}
+
+const MDIWindow::ActionList& MDIWindow::actionList() const
+{
+  return m_actionList;
 }
