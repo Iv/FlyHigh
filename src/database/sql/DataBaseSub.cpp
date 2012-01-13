b0b2eca1c2e28ab890585befc209ae284b880c77
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2012-01-13 19:38:38 +0000

    removed QT3 support
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@1756 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/database/sql/DataBaseSub.cpp
+++ src/database/sql/DataBaseSub.cpp
@@ -18,7 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include <q3sqlcursor.h>
 #include <QDateTime>
 #include <QSqlQuery>
 #include "DataBaseSub.h"
