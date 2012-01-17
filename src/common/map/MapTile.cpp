8be9b5be4796fa7d120d1771436110b23becf5a6
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2012-01-17 20:47:45 +0000

    added GPL
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@1763 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/map/MapTile.cpp
+++ src/common/map/MapTile.cpp
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #include <QColor>
 #include "MapTile.h"
 
@@ -33,18 +53,18 @@ void MapTile::setZoom(int z)
 	if(z < 0)
 	{
 		scale = -z * 2;
-                m_scaledImage = m_image.scaled(m_image.width() * scale,
-                                               m_image.height() * scale,
-                                               Qt::IgnoreAspectRatio,
-                                               Qt::SmoothTransformation);
+    m_scaledImage = m_image.scaled(m_image.width() * scale,
+                                   m_image.height() * scale,
+                                   Qt::IgnoreAspectRatio,
+                                   Qt::SmoothTransformation);
 	}
 	else if(z > 0)
 	{
 		scale = z * 2;
-                m_scaledImage = m_image.scaled(m_image.width() / scale,
-                                               m_image.height() / scale,
-                                               Qt::IgnoreAspectRatio,
-                                               Qt::SmoothTransformation);
+    m_scaledImage = m_image.scaled(m_image.width() / scale,
+                                   m_image.height() / scale,
+                                   Qt::IgnoreAspectRatio,
+                                   Qt::SmoothTransformation);
 	}
 	else
 	{
