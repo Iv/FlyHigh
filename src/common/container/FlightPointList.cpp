e0b7a4a65170e5262806e9d63d7630c83779555d
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2012-11-21 12:28:31 +0000

    added simplify(), which uses Douglas-Peucker algorithm to reduce flight points
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@1948 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/common/container/FlightPointList.cpp
+++ src/common/container/FlightPointList.cpp
@@ -17,7 +17,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
- 
+
 #include "FlightPointList.h"
 
 FlightPointList::FlightPointList()
@@ -54,7 +54,7 @@ int FlightPointList::firstValidFlightData()
 	int index = -1;
 	int lastIndex = (size() - 2);
 	int fpNr;
-	
+
 	for(fpNr=0; fpNr<lastIndex; fpNr++)
 	{
 		if(speedH(fpNr, fpNr+1) > 2.777777)
@@ -63,7 +63,7 @@ int FlightPointList::firstValidFlightData()
 			break;
 		}
 	}
-	
+
 	return index;
 }
 
@@ -72,7 +72,7 @@ int FlightPointList::lastValidFlightData()
 	int index = -1;
 	int lastIndex = (size() - 2);
 	int fpNr;
-	
+
 	for(fpNr=lastIndex; fpNr>0; fpNr--)
 	{
 		if(speedH(fpNr, fpNr+1)  > 0.5)
@@ -81,7 +81,7 @@ int FlightPointList::lastValidFlightData()
 			break;
 		}
 	}
-	
+
 	return index;
 }
 
@@ -91,18 +91,18 @@ double FlightPointList::speedH(uint index1, uint index2)
 	uint nFlightPoints = size();
 	double speedH = - 1.0;
 	double distance;
-	
+
 	if((index1 < nFlightPoints) && (index2 < nFlightPoints))
 	{
 		distance = (double)m_flightPointList[index1].wp.distance(m_flightPointList[index2].wp);
 		deltaTime = (double)m_flightPointList[index1].time.secsTo(m_flightPointList[index2].time);
-		
+
 		if(deltaTime > 0.0)
 		{
 			speedH = (distance / deltaTime);
 		}
 	}
-	
+
 	return speedH;
 }
 
@@ -112,18 +112,18 @@ double FlightPointList::speedV(uint index1, uint index2)
 	uint nFlightPoints = size();
 	double speedV = - 1.0;
 	double deltaAlt;
-	
+
 	if((index1 < nFlightPoints) && (index2 < nFlightPoints))
 	{
 		deltaAlt = (double)m_flightPointList[index2].wp.altitude() - m_flightPointList[index1].wp.altitude();
 		deltaTime = (double)m_flightPointList[index1].time.secsTo(m_flightPointList[index2].time);
-		
+
 		if(deltaTime > 0.0)
 		{
 			speedV = (deltaAlt / deltaTime);
 		}
 	}
-	
+
 	return speedV;
 }
 
@@ -134,11 +134,128 @@ int FlightPointList::duration(uint index1, uint index2)
 	bool valid;
 
 	valid = ((index1 < nFlightPoints) && (index2 < nFlightPoints));
-	
+
 	if(valid)
 	{
 		duration = m_flightPointList[index1].time.secsTo(m_flightPointList[index2].time);
 	}
 
 	return duration;
+}
+
+#define dot(u,v)   ((u).longitude() * (v).longitude() + (u).latitude() * (v).latitude())
+#define norm2(v)   dot(v,v)        // norm2 = squared length of vector
+//#define norm(v)    sqrt(norm2(v))  // norm = length of vector
+#define dist2(u,v)    norm2(u-v)      // distance squared = norm2 of difference
+//#define dist(u,v)     norm(u-v)       // distance = norm of difference
+
+void FlightPointList::simplify(FlightPointList &simpleList)
+{
+  const double epsilon = 0.0001;
+  FlighPointListType vt; // vertex buffer
+  MarkerBuffer marker;  // marker buffer
+  double tol2 = (epsilon * epsilon); // tolerance squared
+  int size;
+  int index;
+  int prev; // misc counters
+  int end;
+
+
+  size = m_flightPointList.size();
+
+  // STAGE 1. Vertex Reduction within tolerance of prior vertex cluster
+  vt.push_back(m_flightPointList[0]);              // start at the beginning
+  end = (size - 1);
+  prev = 0;
+
+  for(index=1; index<size; index++)
+  {
+    if(dist2(m_flightPointList[index].wp, m_flightPointList[prev].wp) < tol2)
+          continue;
+
+    vt.push_back(m_flightPointList[index]);
+    prev = index;
+  }
+
+  if(prev < end)
+  {
+    vt.push_back(m_flightPointList[end]);
+  }
+
+  // STAGE 2. Douglas-Peucker polyline simplification
+  size = vt.size();
+  end = (size - 1);
+  marker.resize(size);
+  marker[0] = true;
+  marker[end] = true;
+  douglasPeucker(vt, 0, end, marker, epsilon);
+
+  // STAGE 3. copy marked vertices to the output simplified polyline
+  for(index=0; index<size; index++)
+  {
+    if(marker[index])
+    {
+      simpleList.add(vt[index]);
+    }
+  }
+}
+
+void FlightPointList::douglasPeucker(const FlighPointListType &ptList, int begin, int end,
+                                   MarkerBuffer &marker, float epsilon) const
+{
+  WayPoint w;
+  WayPoint pb; // base of perpendicular from v[i] to segment
+  WayPoint u = (ptList[end].wp - ptList[begin].wp);   // segment direction vector
+  double maxDist2 = 0;             // distance squared of farthest vertex
+  double tol2 = (epsilon * epsilon);    // tolerance squared
+  double cu = dot(u, u);         // segment length squared
+  double b;
+  double cw;
+  double distSeg2; // distance v[i] to segment squared
+  int index;
+  int maxIndex = begin; // index of vertex farthest from segment
+
+  if(end <= (begin + 1)) // there is nothing to simplify
+      return;
+
+  // test each vertex v[i] for max distance from S
+  // compute using the Feb 2001 Algorithm's dist_Point_to_Segment()
+  for(index=(begin + 1); index<end; index++)
+  {
+    // compute distance squared
+    w = (ptList[index].wp - ptList[begin].wp);
+    cw = dot(w, u);
+
+    if(cw <= 0)
+    {
+      distSeg2 = dist2(ptList[index].wp, ptList[begin].wp);
+    }
+    else if(cu <= cw)
+    {
+      distSeg2 = dist2(ptList[index].wp, ptList[end].wp);
+    }
+    else
+    {
+      b = cw / cu;
+      pb = ptList[begin].wp + u * b;
+      distSeg2 = dist2(ptList[index].wp, pb);
+    }
+
+    // test with current max distance squared
+    if(distSeg2 <= maxDist2)
+        continue;
+
+    // v[i] is a new max vertex
+    maxIndex = index;
+    maxDist2 = distSeg2;
+  }
+
+  if(maxDist2 > tol2) // error is worse than the tolerance
+  {
+    // split the polyline at the farthest vertex from segment
+    marker[maxIndex] = true;      // mark v[maxIndex] for the simplified polyline
+    // recursively simplify the two subpolylines at ptList[maxIndex]
+    douglasPeucker(ptList, begin, maxIndex, marker, epsilon);
+    douglasPeucker(ptList, maxIndex, end, marker, epsilon);
+  }
 }
