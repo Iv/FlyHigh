a9c64f9b5c3666c17217e447d159613fe9ff6b1a
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2013-04-08 19:32:40 +0000

    moved to flyhigh
    git-svn-id: https://svn.code.sf.net/p/flyhigh/code/trunk@2102 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ src/settings/flyhigh/FlyHighSettingsDlg.h
@@ -0,0 +1,83 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef FlyHighSettingsDlg_H
+#define FlyHighSettingsDlg_H
+
+#include <QDialog>
+
+class QTabWidget;
+class QDialogButtonBox;
+class QVBoxLayout;
+class DatabaseWidget;
+class DatabaseParameters;
+class DeviceConnectionWidget;
+class DeviceConnectionParameters;
+
+class FlyHighSettingsDlg: public QDialog
+{
+  public:
+
+    /**
+     * Creates a modal preferences dialog
+     */
+    FlyHighSettingsDlg(QWidget* parent = 0);
+
+    ~FlyHighSettingsDlg();
+
+    /**
+     * Access to the db config
+     */
+    const DatabaseParameters getDBParameters() const;
+
+    /**
+     * Access to the device config
+     */
+    const DeviceConnectionParameters getDeviceConnectionParameters() const;
+
+private:
+
+    /**
+     * Tab container
+     */
+    QTabWidget* m_pTabWidget;
+
+    /**
+     * Ok/Cancel button
+     */
+    QDialogButtonBox* m_pButtonBox;
+
+    /**
+     * Layout manager
+     */
+    QVBoxLayout* m_pLayout;
+
+    /**
+     * Database configuration dialog
+     */
+    DatabaseWidget* m_pDBConfig;
+
+    /**
+     * Device connection dialog
+     */
+    DeviceConnectionWidget* m_pDeviceConfig;
+};
+
+#endif
