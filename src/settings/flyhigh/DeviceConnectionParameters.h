ef8e919deb9ad0472c3269073e43956f0d4ca171
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2013-04-08 20:08:22 +0000

    readded from local copy
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@2112 1728cf9f-2348-4ec0-968f-f422e243bc8e

---
+++ src/settings/flyhigh/DeviceConnectionParameters.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Alex Graf                                       *
+ *   grafal@sourceforge.net                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef DEVICECONNECTIONPARAMETERS_H
+#define DEVICECONNECTIONPARAMETERS_H
+
+#include <QString>
+
+/**
+ * Encapsulates device connection parameters.
+ */
+class DeviceConnectionParameters
+{
+
+public:
+
+  DeviceConnectionParameters(const QString& type,
+                             const QString& port,
+                             const QString& speed);
+
+  DeviceConnectionParameters();
+
+  bool operator==(const DeviceConnectionParameters& other) const;
+  bool operator!=(const DeviceConnectionParameters& other) const;
+
+  void readFromConfig();
+  void writeToConfig() const;
+
+  QString deviceType() const;
+  const QString& port() const;
+  const QString& lineSpeed() const;
+
+  static DeviceConnectionParameters defaultParameters();
+  static DeviceConnectionParameters parametersFromConfig();
+
+private:
+
+  QString m_deviceType;
+  QString m_port;
+  QString m_lineSpeed;
+
+};
+
+#endif // DEVICECONNECTIONPARAMETERS_H
