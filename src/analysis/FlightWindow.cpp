9308650ebdb9520a8f951f249a55e3df700d2729
grafal <grafal@1728cf9f-2348-4ec0-968f-f422e243bc8e>
2014-02-18 14:21:10 +0000

    cosmetics
    git-svn-id: svn://svn.code.sf.net/p/flyhigh/code/trunk@2208 1728cf9f-2348-4ec0-968f-f422e243bc8e

--- src/analysis/FlightWindow.cpp
+++ src/analysis/FlightWindow.cpp
@@ -1,21 +1,21 @@
 /***************************************************************************
-* Copyright (C) 2004 by Alex Graf *
-* grafal@sourceforge.net *
-* *
-* This program is free software; you can redistribute it and/or modify *
-* it under the terms of the GNU General Public License as published by *
-* the Free Software Foundation; either version 2 of the License, or *
-* (at your option) any later version. *
-* *
-* This program is distributed in the hope that it will be useful, *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the *
-* GNU General Public License for more details. *
-* *
-* You should have received a copy of the GNU General Public License *
-* along with this program; if not, write to the *
-* Free Software Foundation, Inc., *
-* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+* Copyright (C) 2004 by Alex Graf                                          *
+* grafal@sourceforge.net                                                   *
+*                                                                          *
+* This program is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published by     *
+* the Free Software Foundation; either version 2 of the License, or        *
+* (at your option) any later version.                                      *
+*                                                                          *
+* This program is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+* GNU General Public License for more details.                             *
+*                                                                          *
+* You should have received a copy of the GNU General Public License        *
+* along with this program; if not, write to the                            *
+* Free Software Foundation, Inc.,                                          *
+* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.                 *
 ***************************************************************************/
 
 #include <QAction>
@@ -462,8 +462,8 @@ void FlightWindow::file_import()
 
       if(olcOptimizer.optimize())
       {
-      // distance
-      flight.setDistance(olcOptimizer.freeDistance(fpIndexList));
+        // distance
+        flight.setDistance(olcOptimizer.freeDistance(fpIndexList));
       }
 
       progDlg.endProgress();
@@ -478,8 +478,8 @@ void FlightWindow::file_import()
       // start
       startPtId = igcParser.flightPointList().firstValidFlightData();
 
-        if(startPtId >= 0)
-        {
+      if(startPtId >= 0)
+      {
         // add UTC offset to start time
         time = igcParser.flightPointList().at(startPtId)->time().addSecs(IFlyHighRC::pInstance()->utcOffset() * 3600);
         flight.setTime(time);
